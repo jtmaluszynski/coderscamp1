93797c22c50e33ba587d0abbb0e165e9da5a0c80
Eliastion <zajackic@wp.pl>
2019-08-09 09:18:19 +0200

    -re-structured folders for off-server navigation -added footer

--- pre-rendering/rip-from-localhost.sh
+++ pre-rendering/rip-from-localhost.sh
@@ -3,4 +3,4 @@
 # crawls localhost and saves the entire webpage
 # creates a fully statis (no server-side rendering) webpage in /pre-rendered folder
 # that version of the website no longer requires interpreting server to run
-wget -r -p //www.makeuseof.com
+wget -r -P pre-rendered/ -p localhost
